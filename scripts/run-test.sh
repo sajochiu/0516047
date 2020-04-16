c82f68ad7499c2079800f9f249073e033888af1f
sajochiu <sajochiu@gmail.com>
2020-04-16 11:39:42 +0800

    lab4

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,2 +1,2 @@
-javac -cp junit-4.12.jar:. PriorityQueueTest.java
-java -cp junit-4.12.jar:hamcrest-core-1.3.jar: org.junit.runner.JUnitCore PriorityQueueTest
+javac -cp lib/junit-4.12.jar:. PriorityQueueTest.java
+java -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar: org.junit.runner.JUnitCore PriorityQueueTest
